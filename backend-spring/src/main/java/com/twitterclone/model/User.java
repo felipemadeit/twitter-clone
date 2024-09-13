c233769c318857af11556f260222154a3111fdcf
felipemadeit <felipemadeit@gmail.com>
2024-09-12 23:57:38 -0500

    Se esta configurando la app para el desarrollo

---
+++ backend-spring/src/main/java/com/twitterclone/model/User.java
@@ -0,0 +1,12 @@
+package com.twitterclone.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class User {
+
+}
