febfd62c1c66a03ced04dd946444ddae55b84e4a
felipemadeit <felipemadeit@gmail.com>
2024-09-22 16:43:05 -0500

    En proceso de conexion form de creación de usuario con el front

--- backend-spring/src/main/java/com/twitterclone/TwitterCloneApplication.java
+++ backend-spring/src/main/java/com/twitterclone/TwitterCloneApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class TwitterCloneApplication {
 
-	public static void main(String[] args) {
+ 	public static void main(String[] args) {
 		SpringApplication.run(TwitterCloneApplication.class, args);
 	}
 
